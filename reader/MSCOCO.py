5d1b83934aba46a8ee0cbc5cc68cd888c11140e0
norikinishida <norikinishida@gmail.com>
2017-04-13 13:38:37 +0900

    fix

--- reader/MSCOCO.py
+++ reader/MSCOCO.py
@@ -2,11 +2,15 @@
 
 import json
 import os
-import sys
 
+import numpy as np
 import scipy.io as sio
 import h5py
+import pyprind
 
+N_TRAIN = 566747
+N_VAL = 25010
+N_TEST = 25010
 
 class MSCOCO(object):
     """
@@ -21,14 +25,17 @@ class MSCOCO(object):
         
         self.images = {"train": [], "val": [], "test": []}
         self.captions = {"train": [], "val": [], "test": []}
-        hdf5Files = {"train": h5py.File(os.path.join(path_vgg_feats, "vgg_feats.train.h5"), "w"),
-                    "val": h5py.File(os.path.join(path_vgg_feats, "vgg_feats.val.h5"), "w"),
-                    "test": h5py.File(os.path.join(path_vgg_feats, "vgg_feats.test.h5"), "w")}
+        hdf5File_train = h5py.File(os.path.join(path_vgg_feats, "vgg_feats.train.h5"), "w")
+        hdf5File_val = h5py.File(os.path.join(path_vgg_feats, "vgg_feats.val.h5"), "w")
+        hdf5File_test = h5py.File(os.path.join(path_vgg_feats, "vgg_feats.test.h5"), "w")
+        hdf5data_train = hdf5File_train.create_dataset("data", shape=(N_TRAIN, 4096), dtype=np.float32) 
+        hdf5data_val = hdf5File_val.create_dataset("data", shape=(N_VAL, 4096), dtype=np.float32) 
+        hdf5data_test = hdf5File_test.create_dataset("data", shape=(N_TEST, 4096), dtype=np.float32) 
 
         with open(os.path.join(path, "..", "karpathy", "coco", "dataset.json")) as f:
             data = json.load(f)
             data = data["images"]
-            for d in data:
+            for d in pyprind.prog_bar(data):
                 img_name = d["filename"]
                 split_name = d["filepath"]
                 split = d["split"]
@@ -45,26 +52,34 @@ class MSCOCO(object):
                         cap = " ".join(cap["tokens"])
                         self.images["train"].append(path_img)
                         self.captions["train"].append(cap)
-                        hdf5Files["train"].create_dataset(str(len(self.images["train"])-1), data=feats)
+                        hdf5data_train[len(self.images["train"])-1,:] = feats
                 elif split == u"val":
                     for cap in captions:
                         cap = " ".join(cap["tokens"])
                         self.images["val"].append(path_img)
                         self.captions["val"].append(cap)
-                        hdf5Files["val"].create_dataset(str(len(self.images["val"])-1), data=feats)
+                        hdf5data_val[len(self.images["val"])-1,:] = feats
                 elif split == u"test":
                     for cap in captions:
                         cap = " ".join(cap["tokens"])
                         self.images["test"].append(path_img)
                         self.captions["test"].append(cap)
-                        hdf5Files["test"].create_dataset(str(len(self.images["test"])-1), data=feats)
+                        hdf5data_test[len(self.images["test"])-1,:] = feats
         
-        for split in ["train", "val", "test"]:
-            hdf5Files[split].flush()
-            hdf5Files[split].close()
+        hdf5File_train.flush()
+        hdf5File_train.close()
+        hdf5File_val.flush()
+        hdf5File_val.close()
+        hdf5File_test.flush()
+        hdf5File_test.close()
+        
+        self.hdf5Files = {
+                "train": h5py.File(os.path.join(path_vgg_feats, "vgg_feats.train.h5"), "r"),
+                "val": h5py.File(os.path.join(path_vgg_feats, "vgg_feats.val.h5"), "r"),
+                "test": h5py.File(os.path.join(path_vgg_feats, "vgg_feats.test.h5"), "r")}
 
     def get_data(self, split):
-        return self.images[split], h5py.File("vgg_feats.%s.h5" % split), self.captions[split]
+        return self.images[split], self.hdf5Files[split]["data"], self.captions[split]
 
 
 # class MSCOCO(object):
@@ -126,4 +141,3 @@ class MSCOCO(object):
 #
 #     def get_data(self, split):
 #         return self.images[split], self.captions[split]
-
