a89eb2e94365089efaf6e231e98046207cea3d8f
norikinishida <norikinishida@gmail.com>
2017-04-30 04:16:02 +0900

    fix

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import reader
+import corpus_reader
 
 def main():
-    msrp = reader.MSRP("/mnt/hdd/dataset/Microsoft-Research-Paraphrase-Corpus")
-    sick = reader.SICK("/mnt/hdd/dataset/SICK/semeval2014-task1")
-    snli = reader.SNLI("/mnt/hdd/dataset/Stanford-Natural-Language-Inference/snli_1.0")
-    sst = reader.SST("/mnt/hdd/dataset/Stanford-Sentiment-Treebank/stanfordSentimentTreebank/trees")
-    mscoco = reader.MSCOCO("/mnt/hdd1/dataset/COCO/", "./")
+    msrp = corpus_reader.MSRP("/mnt/hdd/dataset/Microsoft-Research-Paraphrase-Corpus")
+    sick = corpus_reader.SICK("/mnt/hdd/dataset/SICK/semeval2014-task1")
+    snli = corpus_reader.SNLI("/mnt/hdd/dataset/Stanford-Natural-Language-Inference/snli_1.0")
+    sst = corpus_reader.SST("/mnt/hdd/dataset/Stanford-Sentiment-Treebank/stanfordSentimentTreebank/trees")
+    mscoco = corpus_reader.MSCOCO("/mnt/hdd1/dataset/COCO/coco", "./")
 
     print "Done."
 
