a24d3c071b597b9ff29f281a33821b91fa6a5543
norikinishida <norikinishida@gmail.com>
2017-06-01 11:14:05 +0900

    fix

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ def main():
     snli = corpus_reader.SNLI("/mnt/hdd/dataset/Stanford-Natural-Language-Inference/snli_1.0")
     sst = corpus_reader.SST("/mnt/hdd/dataset/Stanford-Sentiment-Treebank/stanfordSentimentTreebank/trees")
     mscoco = corpus_reader.MSCOCO("/mnt/hdd1/dataset/COCO/coco", "./")
-    babi = corpus_reader.BABI("/mnt/hdd/dataset/bAbI/tasks_1-20_v1-2/en/", task_id=19)
+    babi = corpus_reader.BABI("/mnt/hdd/dataset/bAbI/tasks_1-20_v1-2/en-10k/", task_id=3)
 
     print "Done."
 
