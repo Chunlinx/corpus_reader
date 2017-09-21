f2fef5e29a393b9b1d67a8f3f90141e5e32c15b3
norikinishida <norikinishida@gmail.com>
2017-09-21 09:43:48 +0900

    fix

--- corpus_reader/__init__.py
+++ corpus_reader/__init__.py
@@ -2,6 +2,7 @@ from MSRP import MSRP
 from SICK import SICK
 from SNLI import SNLI
 from SST import SST
+from PDTB2 import PDTB2
 from bAbI import BABI
 from CBT import CBT
 from MSCOCO import MSCOCO
