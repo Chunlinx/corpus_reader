375add24d4be67fe88cd71acc89a6e0dd627ebe6
norikinishida <norikinishida@gmail.com>
2017-09-21 10:38:47 +0900

    fix

--- main.py
+++ main.py
@@ -3,37 +3,41 @@
 import corpus_reader
 
 def main():
-    print "Loading MSRP corpus ..."
+    print("Loading MSRP corpus ...")
     msrp = corpus_reader.MSRP("/mnt/hdd/dataset/Microsoft-Research-Paraphrase-Corpus")
-    print "Done."
+    print("Done.")
     
-    print "Loading SICK corpus ..."
+    print("Loading SICK corpus ...")
     sick = corpus_reader.SICK("/mnt/hdd/dataset/SICK/semeval2014-task1")
-    print "Done."
+    print("Done.")
 
-    print "Loading SNLI corpus ..."
+    print("Loading SNLI corpus ...")
     snli = corpus_reader.SNLI("/mnt/hdd/dataset/Stanford-Natural-Language-Inference/snli_1.0")
-    print "Done."
+    print("Done.")
 
-    print "Loading SST corpus ..."
+    print("Loading SST corpus ...")
     sst = corpus_reader.SST("/mnt/hdd/dataset/Stanford-Sentiment-Treebank/stanfordSentimentTreebank/trees")
-    print "Done."
+    print("Done.")
 
-    print "Loading bAbI corpus ..."
+    print("Loading PDTB2.0 corpus ...")
+    pdtb = corpus_reader.PDTB2("/mnt/hdd/dataset/PDTB2.0/pdtb_v2/data/piped")
+    print("Done.")
+
+    print("Loading bAbI corpus ...")
     babi = corpus_reader.BABI("/mnt/hdd/dataset/bAbI/tasks_1-20_v1-2/en/", task_id=3)
-    print "Done."
+    print("Done.")
     
-    print "Loading CBT corpus ..."
+    print("Loading CBT corpus ...")
     cbt = corpus_reader.CBT("/mnt/hdd/dataset/ChildrensBookTest/CBTest", word_type="NE")
-    print "Done."
+    print("Done.")
 
-    print "Loading MSCOCO dataset ..."
+    print("Loading MSCOCO dataset ...")
     mscoco = corpus_reader.MSCOCO("/mnt/hdd1/dataset/COCO/coco", path_vgg_feats="./")
-    print "Done."
+    print("Done.")
 
-    print "Loading Pascal Sentences dataset ..."
+    print("Loading Pascal Sentences dataset ...")
     pascal = corpus_reader.PascalSentences(path_images="./pascal_sentences/images")
-    print "Done."
+    print("Done.")
 
 if __name__ == "__main__":
     main()
