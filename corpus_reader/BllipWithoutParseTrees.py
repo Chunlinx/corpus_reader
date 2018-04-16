3d8fc29e9efbfed01c56533cd80e08fede0a63c1
norikinishida <norikinishida@gmail.com>
2018-04-16 18:11:32 +0900

    fix

--- corpus_reader/BllipWithoutParseTrees.py
+++ corpus_reader/BllipWithoutParseTrees.py
@@ -20,49 +20,66 @@ class BllipWithoutParseTrees(object):
         pass
 
     def dump(self, path_dir):
+        # e.g., 50 reute9406_007.0356_13
+        # 50 => # of parses
+        # reute9406_007 =>  article id from the North Americal News Text Corpus
+        # 13 => the index of the sentence in the article
         re_head = re.compile(r"([0-9]+) (.*)_([0-9]+)")
         re_tree = re.compile(r"^\(.*\)$")
         dir_list = os.listdir(self.path)
         dir_list.sort()
-        for dir_name in dir_list:
-            print("Processing %s ..." % os.path.join(self.path, dir_name))
-            # Error files: 271, 297, 299
+        for dir_name in pyprind.prog_bar(dir_list):
+            if not os.path.exists(os.path.join(path_dir, dir_name)):
+                os.makedirs(os.path.join(path_dir, dir_name))
+            # Directories that contain error files: 271, 297, 299
             if int(dir_name.split(".")[0]) in [271, 297, 299]:
                 print("Skipped.")
                 continue
-
-            f = open(os.path.join(path_dir, dir_name + ".txt"), "w")
             file_names = os.listdir(os.path.join(self.path, dir_name))
             file_names.sort()
-            for file_name in pyprind.prog_bar(file_names):
-                # Read lines
-                path_file = os.path.join(self.path, dir_name, file_name)
-                bz_file = bz2.BZ2File(path_file)
+            for file_name in file_names:
+                print("Processing %s ..." % os.path.join(self.path, dir_name, file_name))
+                # Read 
+                articles = OrderedDict()
+                article_id_set = set()
+                bz_file = bz2.BZ2File(os.path.join(self.path, dir_name, file_name))
                 lines = bz_file.readlines()
-                # Aggregate raw sentences
-                contents = OrderedDict()
                 for line in lines:
                     # line = line.decode("latin8").strip()
                     line = line.strip()
                     if re_head.match(line):
+                        # 新しい文への切り替え点
                         match = re_head.findall(line)
                         match = match[0]
                         # n_parses = int(match[0])
                         article_id = match[1]
-                        sent_number = int(match[2])
-                        contents["%s %d" % (article_id, sent_number)] = None
+                        sentence_index = int(match[2])
+                        articles["%s/%d" % (article_id, sentence_index)] = None
+                        article_id_set.add(article_id)
                     elif re_tree.match(line):
-                        tree = bllipparser.Tree(line)
-                        tokens = tree.tokens()
-                        raw_sent = " ".join(tokens)
-                        raw_sent = raw_sent.replace("-LRB-", "(").replace("-RRB-", ")")
-                        contents["%s %d" % (article_id, sent_number)] = raw_sent.decode("latin8")
+                        # 文. まだassignしてなければ読み込み，既にしてればスルー.
+                        if articles["%s/%d" % (article_id, sentence_index)] is None:
+                            # まだassignしてなければ読み込む
+                            tree = bllipparser.Tree(line)
+                            tokens = tree.tokens()
+                            raw_sent = " ".join(tokens)
+                            raw_sent = raw_sent.replace("-LRB-", "(").replace("-RRB-", ")")
+                            articles["%s/%d" % (article_id, sentence_index)] = raw_sent.decode("latin8")
+                        else:
+                            # 既にassignしてれば何もしない (計算コストの節約)
+                            continue
                     else:
+                        # 恐らくparseスコア
                         continue
                 # Wtite
-                for key in contents.keys():
-                    f.write("%s\n" % contents[key].encode("utf-8"))
-                f.flush()
-            f.close()
+                for article_id in article_id_set:
+                    with open(os.path.join(path_dir, dir_name, article_id + ".txt"), "w") as f:
+                        for key in articles:
+                            key0, key1 = key.split("/")
+                            if key0 == article_id:
+                                s = articles[key]
+                                # line = "[%s] %s" % (key1, s) # DEBUG
+                                line = s
+                                f.write("%s\n" % line.encode("utf-8"))
 
 
